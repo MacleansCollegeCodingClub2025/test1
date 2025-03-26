81a09771cff8e75b84fecbe013b81694384988ac
Wanwanwanrong <zhang686899@outlook.com>
2025-03-26 16:39:15 +1300

    Create 1.py
    25/3/26

---
+++ 1.py
@@ -0,0 +1,12 @@
+#question 1
+x=int(input("number of minutes"))
+print(x*60)
+#question 2
+y=int(input("number of cards Cruella got"))
+print(y*2)
+#question 3
+z=int(input("number of age in years"))
+print(z*365)
+#question 3.1
+print(z*365)
+print(36500-z*365)
